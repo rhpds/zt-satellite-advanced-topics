65b158fcb593589e2f4156a5614e8e15609e9d4c
Matthew Yee <myee@redhat.com>
2025-09-23 08:43:12 -0700

    Revert "Add wget command to download hello RPM in setup-satellite.sh"
    This reverts commit 338e5b467e5f8cbdf705b74a7ac64390688ec9e9.

--- runtime-automation/01-introduction/setup-satellite.sh
+++ runtime-automation/01-introduction/setup-satellite.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 echo "Starting module called 01-introduction" >> /tmp/progress.log
-wget -P /root https://github.com/myee111/hello-rpm/releases/download/0.0.1/hello-0.0.1-1.el9.noarch.rpm
