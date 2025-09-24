688630a3201b154cde3add1b6d70c8d495ee1f89
Matthew Yee <myee@redhat.com>
2025-09-24 09:44:26 -0700

    Add logging for RHEL 2 setup process in setup-rhel2.sh

--- setup-automation/setup-rhel2.sh
+++ setup-automation/setup-rhel2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 ## Convert the system to Image Mode
+echo "Setting up RHEL 2" >> /tmp/progress.log
+$whoami = $(whoami)
+echo User:$whoami >> /tmp/progress.log
 podman run --rm --privileged -v /dev:/dev -v /var/lib/containers:/var/lib/containers -v /:/target --pid=host --security-opt label=type:unconfined_t quay.io/toharris/rhel-bootc:summit-2025 bootc install to-existing-root --root-ssh-authorized-keys /target/root/.ssh/authorized_keys --acknowledge-destructive
